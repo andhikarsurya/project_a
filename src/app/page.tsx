b0cdd013240161c88ea0070e9a947ae1977b3b07
andhikarsurya <andhikarsurya@gmail.com>
2025-01-10 09:35:24 +0700

    hi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,192 @@
-import Image from "next/image";
-
+"use client";
+import { useState } from "react";
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  let data = [
+    [6, 1],
+    [4, 3],
+    [5, 1],
+    [3, 4],
+    [1, 1],
+    [3, 4],
+    [1, 2],
+  ];
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  let copy_data = [...data];
+  const [copyData, setCopyData] = useState([...data]);
+  const [inputValue, setInputValue] = useState("");
+
+  const sorting_asc = () => {
+    const sortedData = [...copyData].sort((a, b) => {
+      let sumA = a[0] + a[1];
+      let sumB = b[0] + b[1];
+      if (sumA === sumB) {
+        return a[0] - b[0];
+      }
+      return sumA - sumB;
+    });
+    setCopyData(sortedData);
+  };
+  const sorting_desc = () => {
+    const sortedData = [...copyData].sort((a, b) => {
+      let sumA = a[0] + a[1];
+      let sumB = b[0] + b[1];
+      if (sumA === sumB) {
+        return b[0] - a[0];
+      }
+      return sumB - sumA;
+    });
+    setCopyData(sortedData);
+  };
+
+  const reverse = () => {
+    const sortedData = [...copyData].reverse();
+    setCopyData(sortedData);
+  };
+  const RemoveDuplicate = () => {
+    let zzz: number[][] = [];
+  
+    const sortedData = [...copyData].sort((a, b) => {
+      if (a[0] > a[1]) {
+        let temp = a[0];
+        a[0] = a[1];
+        a[1] = temp;
+      }
+      if (b[0] > b[1]) {
+        let temp = b[0];
+        b[0] = b[1];
+        b[1] = temp;
+      }
+  
+      let sumA = a[0] + a[1];
+      let sumB = b[0] + b[1];
+      if (sumA === sumB) {
+        return b[0] - a[0];
+      }
+      return sumB - sumA;
+    });
+  
+    const seen = new Set<string>();
+  
+    for (let i = 0; i < sortedData.length; i++) {
+      const pairString = JSON.stringify(sortedData[i]);
+      
+      if (!seen.has(pairString)) {
+        seen.add(pairString);
+        zzz.push(sortedData[i]);
+      }
+    }
+  
+    setCopyData(zzz);
+  };
+  
+  const RemoveCertainNumber = (a: number) => {
+    const sortedData = [...copyData];
+    let result: number[][] = [];
+    for (let index = 0; index < sortedData.length; index++) {
+      if (sortedData[index][0] + sortedData[index][1] != a) {
+        result.push(sortedData[index]);
+      }
+      setCopyData(result);
+    }
+  };
+  const reset = () => {
+    setCopyData(data);
+  };
+  const countDuplicates = (arr: number[][]) => {
+    let double = 0;
+    for (let index = 0; index < data.length; index++) {
+      if (data[index][0] == data[index][1]) {
+        double++;
+      }
+    }
+    return double;
+  };
+
+  const duplicateCount = countDuplicates(data);
+  const handleInputChange = (e: any) => {
+    setInputValue(e.target.value);
+  };
+  return (
+    <div className="flex flex-col justify-center items-center gap-[1rem] my-[1rem]">
+      <div className="border-2  p-[1rem]">
+        <h1>Source</h1>
+        <div className="flex">
+          {data.map((item, index) => (
+            <div key={index} className="px-2">{`[${item.join(", ")}]`}</div>
+          ))}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+      </div>
+
+      <div className="border-2  p-[1rem] mt-4">
+        <h1>Duplicated</h1>
+        <p>{`There are ${duplicateCount} duplicate entries.`}</p>
+      </div>
+
+      <div className="flex gap-[1rem]">
+        {copyData.map((item, idx) => (
+          <div key={idx}>
+            <div className="flex flex-col border-2 p-[1rem] gap-[1rem]">
+              <div>{item[0]}</div>
+              <div>
+                <hr></hr>
+              </div>
+              <div>{item[1]}</div>
+              <div></div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="flex justify-center gap-[1rem]">
+        <div>
+          <button
+            onClick={sorting_asc}
+            className="bg-blue-800 p-[1rem] rounded-lg"
+          >
+            Sort Asc
+          </button>
+        </div>
+        <div>
+          <button
+            onClick={sorting_desc}
+            className="bg-blue-800 p-[1rem] rounded-lg"
+          >
+            Sort Desc
+          </button>
+        </div>
+        <div>
+          <button onClick={reverse} className="bg-blue-800 p-[1rem] rounded-lg">
+            Flip
+          </button>
+        </div>
+        <div>
+          <button onClick={RemoveDuplicate} className="bg-blue-800 p-[1rem] rounded-lg">
+            Remove dup
+          </button>
+        </div>
+        <div>
+          <button onClick={reset} className="bg-blue-800 p-[1rem] rounded-lg">
+            Reset
+          </button>
+        </div>
+      </div>
+      <div>
+        <input
+          id="input"
+          type="text"
+          placeholder="input number"
+          className="py-[0.3rem] px-[1rem] text-black"
+          value={inputValue}
+          onChange={handleInputChange}
+        />
+      </div>
+      <div>
+        <button
+          onClick={() => RemoveCertainNumber(Number(inputValue))}
+          className="bg-blue-800 p-[1rem] rounded-lg"
         >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          Remove
+        </button>
+      </div>
     </div>
   );
 }
